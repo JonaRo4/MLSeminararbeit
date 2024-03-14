cd25666e07272c529ba9ff4017342a156ffdc18a
JonaRo4 <152287910+JonaRo4@users.noreply.github.com>
2024-03-14 15:00:58 +0100

    Update Prototyp.py

--- Prototyp.py
+++ Prototyp.py
@@ -1,7 +1,8 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 from PIL import Image
-import numpy as np
 
 # Laden des trainierten Modells
 model = load_model("FM.h5")
@@ -13,11 +14,11 @@ class_labels = ['Neu', 'Mittel', 'Defekt']
 def main():
     st.title('Werkzeugklassifizierung')
     
-    # Spalten für Bild und Dateneingaben erstellen
-    col1, col2 = st.beta_columns([2, 1])
+    # Spalten für Bild, Dateneingaben und Plot erstellen
+    col1, col2, col3 = st.beta_columns([2, 1, 1])
     
     # Bild hochladen
-    uploaded_image = col1.file_uploader("Bild von Werkzeug hochladen", type=["jpg", "png", "jpeg"], key="uploaded_image")
+    uploaded_image = col1.file_uploader("Bild von Werkzeug hochladen", type=["jpg", "png", "jpeg"])
     
     if uploaded_image is not None:
         col1.image(uploaded_image, caption='Hochgeladenes Bild', use_column_width=True)
@@ -54,10 +55,36 @@ def main():
             
             # Ergebnisse anzeigen
             st.write('Werkzeugzustand:', predicted_class)
+            
+            # Plot der Eingabedaten
+            plot_data(nutzungszeit, predicted_class)
+
+# Funktion zum Plotten der Eingabedaten
+def plot_data(nutzungszeit, predicted_class):
+    fig, ax = plt.subplots()
+    
+    # Klassenlabels und ihre Positionen definieren
+    labels = ['Neu', 'Mittel', 'Defekt']
+    y_pos = np.arange(len(labels))
+    
+    # Positionen für die Nutzungszeit berechnen
+    x_pos = [nutzungszeit] * len(labels)
+    
+    # Plot erstellen
+    ax.barh(y_pos, x_pos, align='center')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(labels)
+    ax.invert_yaxis()  # Klassenlabels umdrehen
+    ax.set_xlabel('Nutzungszeit (Stunden)')
+    ax.set_title('Eingabedaten')
+    
+    # Ergebnisse anzeigen
+    st.pyplot(fig)
 
 # Streamlit-App starten
 if __name__ == '__main__':
     main()
+
 
 
 
