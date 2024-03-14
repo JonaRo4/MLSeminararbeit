6d018c3c8acb040750d59f5d89fd3a9fcf1a4bf4
JonaRo4 <152287910+JonaRo4@users.noreply.github.com>
2024-03-14 14:33:40 +0100

    Update Prototyp.py

--- Prototyp.py
+++ Prototyp.py
@@ -13,30 +13,33 @@ class_labels = ['Neu', 'Mittel', 'Defekt']
 def main():
     st.title('Werkzeugklassifizierung')
     
+    # Spalten für Bild und Dateneingaben erstellen
+    col1, col2 = st.beta_columns([2, 1])
+    
     # Bild hochladen
-    uploaded_image = st.file_uploader("Bild von Werkzeug hochladen", type=["jpg", "png", "jpeg"])
+    uploaded_image = col1.file_uploader("Bild von Werkzeug hochladen", type=["jpg", "png", "jpeg"])
     
     if uploaded_image is not None:
-        st.image(uploaded_image, caption='Hochgeladenes Bild', use_column_width=True)
+        col1.image(uploaded_image, caption='Hochgeladenes Bild', use_column_width=True)
         
         # Vorschub, Drehzahl, Zustellung und Werkzeugtyp eingeben
-        vorschub = st.number_input('Vorschub in mm/min', min_value=0, step=1)
-        drehzahl = st.number_input('Drehzahl in min-1', min_value=0, step=1)
-        zustellung = st.text_input('Zustellung in mm', value='0.0')
-        werkzeugtyp = st.text_input('Werkzeugtyp eingeben', '')
+        vorschub = col2.number_input('Vorschub in mm/min', min_value=0, step=1)
+        drehzahl = col2.number_input('Drehzahl in min-1', min_value=0, step=1)
+        zustellung = col2.text_input('Zustellung in mm', value='0.0')
+        werkzeugtyp = col2.text_input('Werkzeugtyp eingeben', '')
         
         try:
             zustellung = float(zustellung)
             assert zustellung >= 0  # Überprüfen, dass die Eingabe nicht negativ ist
         except (ValueError, AssertionError):
-            st.error('Bitte geben Sie eine gültige positive Zahl für die Zustellung ein.')
+            col2.error('Bitte geben Sie eine gültige positive Zahl für die Zustellung ein.')
             return
         
         # Nutzungszeit des Werkzeugs eingeben
-        nutzungszeit = st.number_input('Nutzungszeit des Werkzeugs in Stunden', min_value=0.0, step=0.5)
+        nutzungszeit = col2.number_input('Nutzungszeit des Werkzeugs in Stunden', min_value=0.0, step=0.5)
         
         # Klassifizierung des hochgeladenen Bildes
-        if st.button('Klassifizieren'):
+        if col2.button('Klassifizieren'):
             image = Image.open(uploaded_image)
             image = image.resize((224, 224))  # Größe an das Modell anpassen
             
@@ -51,6 +54,11 @@ def main():
             
             # Ergebnisse anzeigen
             st.write('Werkzeugzustand:', predicted_class)
+
+# Streamlit-App starten
+if __name__ == '__main__':
+    main()
+
 
 # Streamlit-App starten
 if __name__ == '__main__':
