25e32fcd5a1085a35ebb8c014b550d62385c245b
JonaRo4 <152287910+JonaRo4@users.noreply.github.com>
2024-03-16 14:43:27 +0100

    Update Abgabe.py

--- Abgabe.py
+++ Abgabe.py
@@ -3,50 +3,46 @@ from PIL import Image
 import tensorflow as tf
 import numpy as np
 
-# Laden des vortrainierten Modells
+# Load pretrained model
 model = tf.keras.models.load_model('AbgabeModell.h5')
 
-# Funktion zur Vorhersage der Werkzeugklasse
+# Function to predict the tool class
 def predict_tool_class(image):
-    # Vorverarbeitung des Bildes
-    image = image.resize((224, 224))  # Anpassen der Bildgröße
-    image = np.array(image) / 255.0  # Normalisierung
-    image = np.expand_dims(image, axis=0)  # Hinzufügen einer Dimension für den Batch
+    try:
+        image = image.resize((224, 224))  # Resize image
+        image = np.array(image) / 255.0  # Normalize
+        image = np.expand_dims(image, axis=0)  # Add batch dimension
+        prediction = model.predict(image)
+        classes = ['Defekt', 'Mittel', 'Neu']
+        predicted_class = classes[np.argmax(prediction)]
+        return predicted_class
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
-    # Vorhersage mit dem Modell
-    prediction = model.predict(image)
-
-    # Klassenbezeichnungen
-    classes = ['Defekt', 'Mittel', 'Neu']
-    predicted_class = classes[np.argmax(prediction)]
-
-    return predicted_class
-
-# Streamlit-App
+# Streamlit App
 def main():
     st.title('Werkzeugklassifizierung')
 
-    # Bild hochladen
     uploaded_image = st.file_uploader("Bild von Werkzeug hochladen", type=["jpg", "jpeg", "png"])
 
     if uploaded_image is not None:
-        # Bild anzeigen
         image = Image.open(uploaded_image)
         st.image(image, caption='Hochgeladenes Bild', use_column_width=True)
 
-        # Zusätzliche Parameter eingeben
         tool_name = st.text_input("Werkzeugname", "")
         processing_time = st.number_input("Bearbeitungsdauer (in Minuten)", min_value=0)
-        material = st.text_input("Werkstoff", "")
+        material = st.text_input("Material des Werkzeugs", "")
 
-        # Klassifizierung vornehmen, wenn der Benutzer auf den Button klickt
         if st.button('Klassifizieren'):
-            tool_class = predict_tool_class(image)
-            st.write('Die Werkzeugklasse ist:', tool_class)
-            st.write('Werkzeugname:', tool_name)
-            st.write('Bearbeitungsdauer:', processing_time, 'Minuten')
-            st.write('Werkstoff:', material)
+            with st.spinner('Klassifizierung läuft...'):
+                tool_class = predict_tool_class(image)
+                st.success('Klassifizierung abgeschlossen!')
+                st.write('Die Werkzeugklasse ist:', tool_class)
+                st.write('Werkzeugname:', tool_name)
+                st.write('Bearbeitungsdauer:', processing_time, 'Minuten')
+                st.write('Material des Werkzeugs:', material)
 
 if __name__ == '__main__':
     main()
+
 
