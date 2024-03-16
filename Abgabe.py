ee0213653bf95f49454d95783eacaf08f999830e
JonaRo4 <152287910+JonaRo4@users.noreply.github.com>
2024-03-16 14:49:16 +0100

    Update Abgabe.py

--- Abgabe.py
+++ Abgabe.py
@@ -9,13 +9,9 @@ model = tf.keras.models.load_model('AbgabeModell.h5')
 # Function to predict the tool class
 def predict_tool_class(image):
     try:
-        print("Resizing image...")
         image = image.resize((224, 224))  # Resize image
-        print("Normalizing image...")
         image = np.array(image) / 255.0  # Normalize
-        print("Expanding dimensions...")
         image = np.expand_dims(image, axis=0)  # Add batch dimension
-        print("Predicting class...")
         prediction = model.predict(image)
         classes = ['Defekt', 'Mittel', 'Neu']
         predicted_class = classes[np.argmax(prediction)]
@@ -33,20 +29,15 @@ def main():
         image = Image.open(uploaded_image)
         st.image(image, caption='Hochgeladenes Bild', use_column_width=True)
 
-        tool_name = st.text_input("Werkzeugname", "")
-        processing_time = st.number_input("Bearbeitungsdauer (in Minuten)", min_value=0)
-        material = st.text_input("Material des Werkzeugs", "")
-
         if st.button('Klassifizieren'):
             with st.spinner('Klassifizierung läuft...'):
                 tool_class = predict_tool_class(image)
                 st.success('Klassifizierung abgeschlossen!')
                 st.write('Die Werkzeugklasse ist:', tool_class)
-                st.write('Werkzeugname:', tool_name)
-                st.write('Bearbeitungsdauer:', processing_time, 'Minuten')
-                st.write('Material des Werkzeugs:', material)
 
 if __name__ == '__main__':
     main()
+
+
 
 
